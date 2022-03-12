733ddc5212dee669b70aeb6b19c267997d2d28b5
Acorneld <acorneld@gmail.com>
2022-03-12 09:25:32 -0700

    G&S on player, main added

--- src/com/skilldistillery/cards/common/Player.java
+++ src/com/skilldistillery/cards/common/Player.java
@@ -1,5 +1,45 @@
 package com.skilldistillery.cards.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+
+	private String name;
+	private int numOfCards;
+	public List <Card> playerHand;
+	
+	public Player(String name) {
+		this.name = name;
+		playerHand = new ArrayList<Card>();
+	}
+
+	
+	// Start G&S ****************************************
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumOfCards() {
+		return numOfCards;
+	}
+
+	public void setNumOfCards(int numOfCards) {
+		this.numOfCards = numOfCards;
+	}
+
+	public List<Card> getPlayerHand() {
+		return playerHand;
+	}
+
+	public void setPlayerHand(List<Card> playerHand) {
+		this.playerHand = playerHand;
+	}
+	//End G&S ****************************************
+
 
 }
