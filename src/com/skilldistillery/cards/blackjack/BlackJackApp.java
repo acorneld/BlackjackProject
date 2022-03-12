71b4a421b238017edd25a0b83e420531a73b41b7
Acorneld <acorneld@gmail.com>
2022-03-12 09:13:23 -0700

    Stubbed out from Animal example

---
+++ src/com/skilldistillery/cards/blackjack/BlackJackApp.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.cards.blackjack;
+
+import java.util.Scanner;
+
+import com.skilldistillery.cards.common.Dealer;
+import com.skilldistillery.cards.common.Player;
+
+public class BlackJackApp {
+	
+	private Player player = new Player();
+	private Dealer dealer = new Dealer();
+	Scanner kb = new Scanner(System.in);
+	boolean stay = false;
+
+}
